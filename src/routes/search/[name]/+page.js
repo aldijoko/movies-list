ce6e990ed7fa72a7c05a0af84887e991fb58cfaf
aldijoko <joko@volantis.io>
2023-02-10 21:28:14 +0700

    comment console

--- src/routes/search/[name]/+page.js
+++ src/routes/search/[name]/+page.js
@@ -4,7 +4,7 @@ export async function load({ fetch, params }) {
     const res = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=70bde69caffef0294416c72116362f3d&language=en-US&query=${params.name}&page=1&include_adult=false`);
     const data = await res.json();
     // console.log(data.results)
-    console.log(data)
+    // console.log(data)
     if(res.ok){
         return {
              searchMovie: data.results
